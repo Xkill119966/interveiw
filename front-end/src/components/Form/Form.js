92880eddcadeb50ea89e91d852d2ce5c4ac60cdc
Xkill119966 <devthureinhtun@gmail.com>
2021-06-30 20:54:17 +0630

    Add Css in Form

--- front-end/src/components/Form/Form.js
+++ front-end/src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import './Form.css'
 export default function Form({ addData }) {
     const initilValue = {
         name: '',
@@ -15,30 +15,32 @@ export default function Form({ addData }) {
     }
 
     return (
-        <form onSubmit={submitForm}>
-            <label htmlFor="name">Name</label>
-            <input
-                type="text"
-                name="name"
-                value={data.name}
-                onChange={e => setData({ ...data, name: e.target.value })}
-            />
-            <br />
-            <label htmlFor="name">Age</label>
-            <input
-                type="number"
-                name="age"
-                value={data.age}
-                onChange={e => setData({ ...data, age: e.target.value })}
-            />
-            <br />
+        <div>
+            <form onSubmit={submitForm}>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    value={data.name}
+                    onChange={e => setData({ ...data, name: e.target.value })}
+                />
+                <br />
+                <label htmlFor="name">Age</label>
+                <input
+                    type="number"
+                    name="age"
+                    value={data.age}
+                    onChange={e => setData({ ...data, age: e.target.value })}
+                />
+                <br />
 
-            <input type="radio" value="M" name="gender" checked={data.gender === 'M'} onChange={e => setData({ ...data, gender: e.target.value })} /> Male
-            <input type="radio" value="F" name="gender" checked={data.gender === 'F'} onChange={e => setData({ ...data, gender: e.target.value })} /> Female
-            <br />
-            <button className="btn btn-default" type="submit">
-                Submit
-        </button>
-        </form>
+                <label htmlFor="gender">Gender</label>
+                <input type="radio" value="M" name="gender" checked={data.gender === 'M'} onChange={e => setData({ ...data, gender: e.target.value })} /> Male
+                <input type="radio" value="F" name="gender" checked={data.gender === 'F'} onChange={e => setData({ ...data, gender: e.target.value })} /> Female
+                <br />
+                <input type="submit" value="Submit" />
+
+            </form>
+        </div>
     )
 }
